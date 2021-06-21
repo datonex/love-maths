2476b570b13a72e2a3422006fc8af9e679496765
Tanya G <cgomwe@ymail.com>
2021-06-21 13:04:35 +0100

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// define function to run the game
+function runGame() {}
+
+// define function to check the answer
+function checkAnswer() {}
+
+// define a function to perform the calculation and return the correct answer
+function calculateCorrectAnswer() {}
+
+// define function to increase the score when it's correct
+function incrementScore() {}
+
+// define function to increase the score when it's incorrect
+function incrementWrongAnswer() {}
+
+// define function to show addition, subtraction, multiplication and division questions
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
+
+function displayDivideQuestion() {}
